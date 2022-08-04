291cd04d249e8426a22f1973091daf780cfa2809
jore <jorge.salazar@cornershopapp.com>
2022-08-04 10:33:40 -0400

    feat(tests): Created test for update_part view. Mirrored "production" (local) sqlite database. Deleted data on mirrored db for testing purposes, validated that production data is not touched.

--- parts_api/tests/test_part.py
+++ parts_api/tests/test_part.py
@@ -1,11 +1,30 @@
-from django.test import TestCase
-import sqlite3
-from parts_api import views
-from parts_api.tests.utils import HttpRequest, HttpResponse
+from django.test import Client, TestCase
+from django.urls import reverse
+from rest_framework import status
 
-connection = sqlite3.connect("db.sqlite3")
+from parts_api.models import Part
+from parts_api.tests.factories import TestParts
 
 
 class PartViewTests(TestCase):
+    def setUp(self):
+        Part.objects.all().delete()
+
+        self.parts = TestParts.create_batch(5)
+        self.conn = Client()
+
     def test_update_part(self):
-        pass  # TODO write test
+        test_part = self.parts[0]
+
+        part_id = test_part.id
+        previous_is_active = test_part.is_active
+
+        endpoint = reverse("part-update", kwargs={"part_id": part_id})
+
+        payload = "{\"is_active\": \"False\"}"
+        response = self.conn.put(endpoint, payload)
+
+        post_is_active = Part.objects.get(id=part_id).is_active
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(previous_is_active, post_is_active)
