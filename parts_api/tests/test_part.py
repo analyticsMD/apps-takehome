7336db4cda2af99a642aa01e8815bfff440e2e41
jore <jorge.salazar@cornershopapp.com>
2022-08-04 16:46:16 -0400

    tests(view): Added tests for 404 and 500 errors on update_part view.

--- parts_api/tests/test_part.py
+++ parts_api/tests/test_part.py
@@ -13,7 +13,7 @@ class PartViewTests(TestCase):
         self.parts = TestParts.create_batch(5)
         self.conn = Client()
 
-    def test_update_part(self):
+    def test_update_part__200(self):
         test_part = self.parts[0]
 
         part_id = test_part.id
@@ -28,3 +28,24 @@ class PartViewTests(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotEqual(previous_is_active, post_is_active)
+
+    def test_update_part__404(self):
+        max_id = max([part.id for part in self.parts])
+        endpoint = reverse("part-update", kwargs={"part_id": max_id + 1})
+
+        payload = "{\"is_active\": \"False\"}"
+        response = self.conn.put(endpoint, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_part__500(self):
+        test_part = self.parts[0]
+
+        part_id = test_part.id
+
+        endpoint = reverse("part-update", kwargs={"part_id": part_id})
+
+        payload = "{\"random_column\": \"False\"}"
+        response = self.conn.put(endpoint, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
