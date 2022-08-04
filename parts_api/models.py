f8df57352b2de75fb8b0df109b5ff8b72018951d
jore <jorge.salazar@cornershopapp.com>
2022-08-04 10:24:57 -0400

    feat(model): Added Part as a model not managed by Django. Kept first migration logic and treated 0002 migration as if the database already has the table, managed by another system. This won't execute SQL on the database.

--- parts_api/models.py
+++ parts_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Part(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=150)
+    sku = models.CharField(max_length=30)
+    description = models.CharField(max_length=1024)
+    weight_ounces = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        managed = False
+        db_table = "part"
